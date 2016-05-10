e804c5de8718dcf131552d982d49158d99dbc3f0
Amanda Futemma <amanda@duxinnovation.com>
2016-05-10 18:59:01 +0000

    ITS OFFICIAL

--- webapp/controller/ocorr_form.controller.js
+++ webapp/controller/ocorr_form.controller.js
@@ -1,6 +1,28 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"], function(BaseController) {
-    
-    
-    
-    
+	"use strict";
+
+	return BaseController.extend("generated.app.controller.ocorr_form", {
+
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("ocorr_form").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+
+		},
+		handleRouteMatched: function(oEvent) {
+			var params = {};
+			if (oEvent.mParameters.data.context || oEvent.mParameters.data.masterContext) {
+				this.sContext = oEvent.mParameters.data.context;
+				this.sMasterContext = oEvent.mParameters.data.masterContext;
+
+				if (!this.sContext) {
+					this.getView().bindElement("/" + this.sMasterContext, params);
+				} else {
+					this.getView().bindElement("/" + this.sContext, params);
+				}
+
+			}
+
+		}
+
+	});
 });
